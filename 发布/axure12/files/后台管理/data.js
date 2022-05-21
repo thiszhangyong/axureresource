be3f320edaf35c35839ddc7714ce1e3d7b084fb2
自在侯 <thisweb@163.com>
2022-05-21 11:33:08 +0800

    应邮件要求删除此原型发布文件

--- 发布/axure12/files/后台管理/data.js
+++
@@ -1,7 +0,0 @@
-﻿$axure.loadCurrentPage(
-(function() {
-    var _ = function() { var r={},a=arguments; for(var i=0; i<a.length; i+=2) r[a[i]]=a[i+1]; return r; }
-    var _creator = function() { return _(b,c,d,e,f,g,h,g,i,[j],k,_(l,m,n,o,p,q,r,_(),s,_(t,u,v,w,x,_(y,z,A,B),C,null,D,w,E,w,F,G,H,null,I,J,K,L,M,N,O,J),P,_(),Q,_(),R,_(S,[])),T,_(),U,_());}; 
-var b="url",c="后台管理.html",d="generationDate",e=new Date(1571638728377.54),f="isCanvasEnabled",g=false,h="isAdaptiveEnabled",i="variables",j="OnLoadVariable",k="page",l="packageId",m="e86d3d0174c04839992cd00e6420b81c",n="type",o="Axure:Page",p="name",q="后台管理",r="notes",s="style",t="baseStyle",u="627587b6038d43cca051c114ac41ad32",v="pageAlignment",w="near",x="fill",y="fillType",z="solid",A="color",B=0xFFFFFFFF,C="image",D="imageHorizontalAlignment",E="imageVerticalAlignment",F="imageRepeat",G="auto",H="favicon",I="sketchFactor",J="0",K="colorStyle",L="appliedColor",M="fontName",N="Applied Font",O="borderWidth",P="adaptiveStyles",Q="interactionMap",R="diagram",S="objects",T="masters",U="objectPaths";
-return _creator();
-})());
